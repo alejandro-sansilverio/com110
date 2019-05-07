e1e63947426761788574c2558cc4caa2cdcecccf
alejandro-sansilverio <50168555+alejandro-sansilverio@users.noreply.github.com>
2019-05-07 14:36:54 -0300

    Create maxmin.c

---
+++ maxmin.c
@@ -0,0 +1,29 @@
+
+#include <stdio.h>
+
+int maxmin(int n, int *pmax, int *pmin){
+    int i, num;
+    printf("Digite o primeiro numero:");
+    scanf("%d", pmax);
+    *pmin=*pmax;
+    for(i=2;i<=n;i++){
+        printf("Digite o %d numero:", i);
+        scanf("%d", &num);
+        if(num>*pmax){
+           *pmax=num; 
+        }else if(num<*pmin){
+            *pmin=num;
+        }else ;
+    }
+    return;
+}
+
+int main()
+{
+    int n, max, min;
+    printf("Digite a quantidade de numeros a serem lidos:");
+    scanf("%d", &n);
+    maxmin(n, &max, &min);
+    printf("\nO valor max eh %d e o valor minimo %d", max, min);
+    return 0;
+}
