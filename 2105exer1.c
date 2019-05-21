8b3be1785b12c43e74a1d1e108cea0c78cd066e0
alejandro-sansilverio <50168555+alejandro-sansilverio@users.noreply.github.com>
2019-05-21 13:56:53 -0300

    Create 2105exer1.c

---
+++ 2105exer1.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+void preenchevetor(int numeros[])
+{
+  int i;
+  for(i=0;i<10;i++)
+  {
+    printf("Digite o %d valor:", i+1);
+    scanf("%d", &numeros[i]);
+  }
+  return ;
+}
+
+void somaPares(*psoma, numeros[])
+{
+  int i;
+  for(i=0;i<10;i++)
+  {
+    if(numeros[i]%2==0) *psoma = *psoma + numeros[i];
+  }
+}
+
+int main()
+{
+  int numeros[10], soma=0;
+  preenchevetor(numeros);
+  somaPares(&soma, numeros);
+  printf("A soma dos vetores eh %d ", soma);
+}
