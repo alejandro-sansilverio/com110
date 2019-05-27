b26ef5554eb362c884fb26d8f524dbacce05b9c9
alejandro-sansilverio <50168555+alejandro-sansilverio@users.noreply.github.com>
2019-05-27 10:47:11 -0300

    Create matexer1.c

---
+++ matexer1.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+void preenchemat(int mat[][])
+{
+  int l, c;
+  for(l=0;l<5;l++)
+  {
+    for(c=0;c<8;c++)
+    {
+      printf("Digite um valor inteiro:");
+      scanf("%d", &mat[l][c]);
+    }
+  }
+  return;
+}
+
+void imprime(int mat[][])
+{
+  int l, c;
+  for(l=0;l<5;l++)
+  {
+    for(c=0;c<8;c++)
+    {
+      printf(" %d ", mat[l][c]);
+    }
+    printf("\n");
+  }
+  return;
+}
+
+int main()
+{
+  int mat[5][8];
+  preenchemat(mat);
+  imprime(mat);
+}
