92b1136d9e787f8b8b1eab3305b71c1a514a2ef1
alejandro-sansilverio <50168555+alejandro-sansilverio@users.noreply.github.com>
2019-05-27 18:04:30 -0300

    Update matexer4.c

--- matexer4.c
+++ matexer4.c
@@ -5,15 +5,47 @@ void preenchemat(int mat[][5])
 {
   for(l=0;l<5;l++)
   {
+    printf("Preenchendo a %d linha\n", l+1);
     for(c=0;c<5;c++)
     {
-      printf("Digite");
+      printf("Digite o %d valor:", c+1);
+      scanf("%d", &mat[l][c]);
     }
   }
+}
+
+void imprimeDp(int mat[][5])
+{
+  printf("\nDiagonal principal\n");
+  for(l=0;l<5;l++)
+  {
+    for(c=0;c<5;c++)
+    {
+      if(l==c) printf("%d ", mat[l][c]);
+      else;
+    }
+  }
+  return;
+}
+
+void imprimeDs(int mat[][5])
+{
+  printf("\nDiagonal Secundaria\n");
+  for(l=0;l<5;l++)
+  {
+    for(c=0;c<5;c++)
+    {
+      if(l+c==4) printf("%d ", mat[l][c]);
+      else;
+    }
+  }
+  return;
 }
 
 int main()
 {
   int mat[5][5];
   preenchemat(mat);
+  imprimeDp(mat);
+  imprimeDs(mat);
 }
