4c9d530a1e20a1fe867f68533c9d10c273b839e0
alejandro-sansilverio <50168555+alejandro-sansilverio@users.noreply.github.com>
2019-05-27 11:59:33 -0300

    Create matexer4.c

---
+++ matexer4.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int l, c;
+
+void preenchemat(int mat[][5])
+{
+  for(l=0;l<5;l++)
+  {
+    for(c=0;c<5;c++)
+    {
+      printf("Digite");
+    }
+  }
+}
+
+int main()
+{
+  int mat[5][5];
+  preenchemat(mat);
+}
