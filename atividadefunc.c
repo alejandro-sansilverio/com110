fb865eeb302a15a468cc47ed42c44bcc193527c6
alejandro-sansilverio <50168555+alejandro-sansilverio@users.noreply.github.com>
2019-06-03 12:02:48 -0300

    Update atividadefunc.c

--- atividadefunc.c
+++ atividadefunc.c
@@ -13,7 +13,7 @@ void funcA(int estoque[][4],int veta[])
 
 int funcB(int estoque[][5], int c)
 {
-  int m;
+  int m=1;
   for(l=1;l<4;l++)
   {
     if(estoque[l][c]>estoque[l-1][c]) m=l+1;
@@ -24,13 +24,39 @@ int funcB(int estoque[][5], int c)
 
 int funcC(int estoque[][5], int c)
 {
-  menor;
-  
+  menor=1;
+  for(l=1;l<4;l++)
+  {
+   if(estoque[l][c]<estoque[l-1][c]) menor = l+1;
+    else;
+  }
+  return;
+}
+
+void funcD(int estoque[][5], int vcusto[]) 
+{
+  for(c=0;c<4;c++)
+  {
+   for(l=0;l<4;l++)
+   {
+      vcusto[c] = vcusto[c] + estoque[l][c]; 
+   }
+    vcusto[c] = vcusto[c]*estoque[4][c];
+  }
+  return;
+}
+
+void funcE(int estoque[][5], int vcusto[], int veta[])
+{
+ for(l=0;l<4;l++)
+ {
+  vcusto[l] = veta[l]*estoque[4] 
+ }
 }
 
 int main()
 {
-  int estoque[5][4], veta[4];
+  int estoque[5][4], veta[4], vcustop[4], vcustoa[4];
   for(l=0;l<5;l++)
   {
   printf("INFORMACOES CORRESPONDENTES AO %d ARMAZEM", l+1);
@@ -50,4 +76,6 @@ int main()
   funcA(veta, estoque);
   funcB(estoque, 1);
   funcC(estoque, 3);
+  funcD(estoque, vcustop);
+  funcE(estoque, vcustoa, veta);
 }
