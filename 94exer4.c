4d41818a91f6a124ad5b53783bd51d9051e5db5c
alejandro-sansilverio <50168555+alejandro-sansilverio@users.noreply.github.com>
2019-06-04 15:15:29 -0300

    Create 94exer4.c

---
+++ 94exer4.c
@@ -0,0 +1,13 @@
+Escreva um programa em C que leia uma lista de N cadeias de caractere de tamanho
+máximo igual a 50 e armazene a quantidade de caractere de cada cadeia em um vetor de
+inteiros. A posição i do vetor de inteiros deve armazenar o tamanho da i-ésima cadeia
+digitada. N deve ser fornecido pelo usuário (valor máximo = 10). Imprima o vetor de
+tamanhos. 
+
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+  
+}
